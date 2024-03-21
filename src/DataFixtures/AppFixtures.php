48e93565b64e677964c1afde14594fc4deb58dd3
efouba <giefouba@gmail.com>
2024-03-21 13:51:43 +0100

    securite de l'API

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,16 +4,38 @@ namespace App\DataFixtures;
 
 use App\Entity\Author;
 use App\Entity\Book;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private $userPasswordHasher;
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+     $this->userPasswordHasher=$userPasswordHasher;   
+    }
+    
     public function load(ObjectManager $manager): void
     {
+        //Creation d'un user 
+        $user=new User();
+        $user->setEmail("user@bookapi.com");
+        $user->setRoles(["ROLE_USER"]);
+        $user->setPassword($this->userPasswordHasher->hashPassword($user,"password"));
+        $manager->persist($user);
+
+        //Creation du conpte administrateur
+        $useradmin=new User();
+        $useradmin->setEmail("admin@bookapi.com");
+        $useradmin->setRoles(["ROLE_ADMIN"]);
+        $useradmin->setPassword($this->userPasswordHasher->hashPassword($useradmin,"password"));
+        $manager->persist($useradmin);
+
         // Création des auteurs.
-    $listAuthor = [];
-    for ($i = 0; $i < 10; $i++) {
+        $listAuthor = [];
+        for ($i = 0; $i < 10; $i++) {
         // Création de l'auteur lui-même.
         $author = new Author();
         $author->setFirstName("Prénom " . $i);
@@ -21,7 +43,7 @@ class AppFixtures extends Fixture
         $manager->persist($author);
         // On sauvegarde l'auteur créé dans un tableau.
         $listAuthor[] = $author;
-    }
+        }
 
         for ($i=0; $i <20 ; $i++) { 
             
